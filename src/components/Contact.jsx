af4a55163ca35a5e09d8b3eaac0ff5dcfb4409d8
MDSaad1313 <mohammadsaadmujawar313@gmail.com>
2026-01-08 17:59:51 +0530

    Contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+
+function Contact() {
+  return (
+    <section id="contact" className="py-5 bg-light">
+      <Container className="text-center">
+        <h2 className="fw-bold">Contact</h2>
+        <p>Email: saadmujawar2667@gmail.com</p>
+        <p>Phone: +91 7391942667</p>
+      </Container>
+    </section>
+  );
+}
+
+export default Contact;
